d50d30aa4da7706e5f2a5b26b629be3f4c2c021b
Bryce <dbgtrulz@optonline.net>
2015-04-30 17:24:22 -0400

    Added untracked files...

---
+++ Apps/BugginOut/data/bots/BallBot.lua
@@ -0,0 +1,16 @@
+--[[
+	init - called once when the bot is created, this sets up the local
+	variables and performs other initialization procedures.
+--]]
+function init()
+	return 0;
+end
+
+--[[
+	think - called once per frame, this is where the bot performs its
+	decision-making. Note that we might not actually do any thinking each
+	frame, depending on the value of cyclesRemainingBeforeThinking.
+--]]
+function think(player)
+	return 0;
+end
